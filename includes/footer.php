816a3f2b62f73629a030dfe0ec08f8b51ed09ac3
LouisseKen10 <louisseken10@gmail.com>
2025-12-07 01:35:23 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<?php
+echo "<p>Created by: Louisse Ken Tiosej0 " . date("29/11/2025") . " bubotika.com</p>";
+?>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<?php
+echo '
+<header>
+    <h1>Welcome to Bu-Botika Store</h1>
+    <p><i>Kung saan bubuti ka</i></p>
+
+    <nav>
+        <a href="index.php">SCHEDULE</a>
+        <a href="medicines.php">MEDICINES</a>
+        <a href="price.php">PRICE</a>
+        <a href="total.php">TOTAL</a>
+    </nav>
+</header>
+';
+?>
